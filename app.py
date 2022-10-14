9aaaaf4b35eb4984656a6a27b9b4bfa5645da489
Ashmhmd <ashfak25321@gmail.com>
2022-10-14 18:28:21 +0530

    changed procfile

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify,request
 import time
+
 app = Flask(__name__);
+
 @app.route("/bot", methods=["POST"])
 def response():
     query = dict(request.form)['query']
     res = query + " " + time.ctime()
     return jsonify({"response" : res})
+
 if __name__=="__main__":
     app.run(host="0.0.0.0",)
