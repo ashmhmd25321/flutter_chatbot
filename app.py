e2b713e55ab94953f78969967b6f33ae66ceb87c
Ashmhmd <ashfak25321@gmail.com>
2023-05-01 13:35:19 +0530

    changed approute url

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
 import time
 import nltk
+
 nltk.download('popular')
 from nltk.stem import WordNetLemmatizer
+
 lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
 
 from keras.models import load_model
+
 model = load_model('model.h5')
 import json
 import random
+
 intents = json.loads(open('data.json').read())
-words = pickle.load(open('texts.pkl','rb'))
-classes = pickle.load(open('labels.pkl','rb'))
+words = pickle.load(open('texts.pkl', 'rb'))
+classes = pickle.load(open('labels.pkl', 'rb'))
 import tensorflow as tf
 
 with tf.device('/CPU:0'):
-  a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
-  b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
-  # Any additional tf code placed in this block will be executed on the CPU
+    a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+    b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+    # Any additional tf code placed in this block will be executed on the CPU
+
 
 def clean_up_sentence(sentence):
     # tokenize the pattern - split words into array
@@ -27,28 +32,30 @@ def clean_up_sentence(sentence):
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
+
 # return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
 
 def bow(sentence, words, show_details=True):
     # tokenize the pattern
     sentence_words = clean_up_sentence(sentence)
     # bag of words - matrix of N words, vocabulary matrix
-    bag = [0]*len(words)  
+    bag = [0] * len(words)
     for s in sentence_words:
-        for i,w in enumerate(words):
-            if w == s: 
+        for i, w in enumerate(words):
+            if w == s:
                 # assign 1 if current word is in the vocabulary position
                 bag[i] = 1
                 if show_details:
-                    print ("found in bag: %s" % w)
-    return(np.array(bag))
+                    print("found in bag: %s" % w)
+    return (np.array(bag))
+
 
 def predict_class(sentence, model):
     # filter out predictions below a threshold
-    p = bow(sentence, words,show_details=False)
+    p = bow(sentence, words, show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
-    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
     # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
@@ -56,14 +63,16 @@ def predict_class(sentence, model):
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
     return return_list
 
+
 def getResponse(ints, intents_json):
     tag = ints[0]['intent']
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
-        if(i['tag']== tag):
+        if (i['tag'] == tag):
             result = random.choice(i['responses'])
             break
     return result
+
 
 def chatbot_response(msg):
     ints = predict_class(msg, model)
@@ -76,14 +85,16 @@ from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 app.static_folder = 'static'
 
-@app.route("/get", methods=["POST"])
+
+@app.route("/bot", methods=["POST"])
 def response():
     query = dict(request.form)['query']
-    #res = "Hey BRO" + time.ctime()
-    #return jsonify({"response" : res})
-    #userText = request.args.get('msg')
+    # res = "Hey BRO" + time.ctime()
+    # return jsonify({"response" : res})
+    # userText = request.args.get('msg')
     res = chatbot_response(query)
-    return jsonify({"response" : res})
+    return jsonify({"response": res})
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",)
+    app.run(host="0.0.0.0")
